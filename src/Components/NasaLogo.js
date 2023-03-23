c6b0ba3534449da149c9a92ab1d3081f35119669
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-22 18:28:22 -0700

    add wireframe to Design folder

--- src/Components/NasaLogo.js
+++ src/Components/NasaLogo.js
@@ -3,15 +3,21 @@ import styled from "styled-components";
 
 const Logo = styled.div`
     padding: 20px 2px;
-    background-color: none;
-    width: 10%;
+    background-color: lightsteelblue;
+    width: 100%;
     margin: 10px;
+    display: flex;
+    color: midnightblue;
 `;
 
 const NasaLogo = () => {
     return (
         <Logo> 
             <img src="../../nasa-logo-web-rgb.png" alt='NASA logo'></img>
+            <div className="header">
+                <h1>NASA</h1>
+                <h3>Photo of the Day</h3>
+            </div>
         </Logo>
        
     );
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,15 +1,60 @@
 import React from 'react';
+import styled from "styled-components";
 
+const NasaPhotoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Explanation = styled.p`
+    max-width: 60%;
+    text-align: justify;
+`;
+
+const NasaImage = styled.img`
+    max-width: 80%;
+    border-radius: 5%;
+`;
+
+const PhotoButton = styled.button`
+   margin: 6px;
+   padding: 10px 40px;
+   border-radius: 20px;
+   color: midnightblue;
+   font-weight: bold;
+`;
+
+const PhotoButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+`;
+const Caption = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 75%;
+    font-size: .7rem;
+    margin: 10px 0 0 0;
+`;
 const NasaPhoto = (props) => {
     return (
-        <div className='nasa-photo-wrapper'>
+        <NasaPhotoWrapper className='nasa-photo-wrapper'>
+           <div>
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
-            <img src={props.photo.hdurl} alt="NASA_photo"></img>
-            <p>Copyright: {props.photo.copyright}</p>
-            <p className='explanation'>{props.photo.explanation}</p>
-        </div>
-
+           </div>
+            <NasaImage src={props.photo.hdurl} alt="NASA_photo"></NasaImage>
+            <Caption>
+                <p>Copyright: {props.photo.copyright}</p>
+                <PhotoButtonWrapper className='PhotoButtonWrapper'>
+                    <PhotoButton> Like </PhotoButton>
+                    <PhotoButton> Share </PhotoButton>
+                </PhotoButtonWrapper>
+            </Caption>
+            <Explanation className='explanation'>{props.photo.explanation}</Explanation>
+        </NasaPhotoWrapper>
     );
 } 
 
